02a5c8185c4bbc54e7a5bf7f7d1820b7aa28083c
Arjan van Eersel <arjanvaneersel@gmail.com>
2020-11-24 11:32:40 +0200

    Abstract sync functionality

--- business/node/serve.go
+++ business/node/serve.go
@@ -12,6 +12,78 @@ import (
 	"google.golang.org/grpc"
 )
 
+func (n *Node) sync() {
+	wg := sync.WaitGroup{}
+
+	for _, peer := range n.peers {
+		p, err := peer.Dial()
+		if err != nil {
+			// Mark peer as inactive
+			n.UpdatePeer(peer, true)
+			continue
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), n.RequestTimeout)
+		defer cancel()
+
+		stream, err := p.client.Sync(ctx)
+		if err != nil {
+			n.logger.Printf("Ping request failed: %v", err)
+		}
+
+		joinedPeers := n.JoinedPeers()
+		faultyPeers := n.FaultyPeers()
+
+		wg.Add(2)
+		go func(stream networking.NetworkingService_SyncClient, joinedPeers []*Peer, faultyPeers []*Peer) {
+			for _, peer := range joinedPeers {
+				if err := stream.Send(&networking.Node{Id: peer.id, Addr: peer.addr, Status: networking.Node_NODE_STATUS_JOINED}); err != nil {
+					n.logger.Printf("Failed to send to stream: %s", err)
+					continue
+				}
+			}
+
+			for _, peer := range faultyPeers {
+				if err := stream.Send(&networking.Node{Id: peer.id, Addr: peer.addr, Status: networking.Node_NODE_STATUS_FAILED}); err != nil {
+					n.logger.Printf("Failed to send to stream: %s", err)
+					continue
+				}
+			}
+
+			wg.Done()
+		}(stream, joinedPeers, faultyPeers)
+
+		go func(stream networking.NetworkingService_SyncClient) {
+			for {
+				msg, err := stream.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					n.logger.Printf("Stream error: %v", err)
+				}
+
+				switch msg.Status {
+				case networking.Node_NODE_STATUS_JOINED:
+					n.AddPeer(&Peer{id: msg.Id, addr: msg.Addr})
+				case networking.Node_NODE_STATUS_FAILED:
+					if n.HasPeer(msg.Id) {
+						n.RemovePeer(msg.Id)
+					}
+				default:
+					n.logger.Printf("Unexpected status: %v", msg.Status)
+				}
+			}
+			wg.Done()
+		}(stream)
+
+		wg.Wait()
+		stream.CloseSend() // TODO: Should perhaps be moved to sending goroutine.
+
+		n.EmptyNewAndfaultyPeers()
+	}
+}
+
 // Serve starts the server and blocks on server requests.
 // It will also initialize the periodic ping requests to check whether
 // peers are still alive.
@@ -45,76 +117,7 @@ func (n *Node) Serve() error {
 		for {
 			select {
 			case <-t.C:
-				// TODO: Abstraction
-				wg := sync.WaitGroup{}
-
-				for _, peer := range n.peers {
-					p, err := peer.Dial()
-					if err != nil {
-						// Mark peer as inactive
-						n.UpdatePeer(peer, true)
-						continue
-					}
-
-					ctx, cancel := context.WithTimeout(context.Background(), n.RequestTimeout)
-					defer cancel()
-
-					stream, err := p.client.Sync(ctx)
-					if err != nil {
-						n.logger.Printf("Ping request failed: %v", err)
-					}
-
-					joinedPeers := n.JoinedPeers()
-					faultyPeers := n.FaultyPeers()
-
-					wg.Add(2)
-					go func(stream networking.NetworkingService_SyncClient, joinedPeers []*Peer, faultyPeers []*Peer) {
-						for _, peer := range joinedPeers {
-							if err := stream.Send(&networking.Node{Id: peer.id, Addr: peer.addr, Status: networking.Node_NODE_STATUS_JOINED}); err != nil {
-								n.logger.Printf("Failed to send to stream: %s", err)
-								continue
-							}
-						}
-
-						for _, peer := range faultyPeers {
-							if err := stream.Send(&networking.Node{Id: peer.id, Addr: peer.addr, Status: networking.Node_NODE_STATUS_FAILED}); err != nil {
-								n.logger.Printf("Failed to send to stream: %s", err)
-								continue
-							}
-						}
-
-						wg.Done()
-					}(stream, joinedPeers, faultyPeers)
-
-					go func(stream networking.NetworkingService_SyncClient) {
-						for {
-							msg, err := stream.Recv()
-							if err == io.EOF {
-								break
-							}
-							if err != nil {
-								n.logger.Printf("Stream error: %v", err)
-							}
-
-							switch msg.Status {
-							case networking.Node_NODE_STATUS_JOINED:
-								n.AddPeer(&Peer{id: msg.Id, addr: msg.Addr})
-							case networking.Node_NODE_STATUS_FAILED:
-								if n.HasPeer(msg.Id) {
-									n.RemovePeer(msg.Id)
-								}
-							default:
-								n.logger.Printf("Unexpected status: %v", msg.Status)
-							}
-						}
-						wg.Done()
-					}(stream)
-
-					wg.Wait()
-					stream.CloseSend() // TODO: Should perhaps be moved to sending goroutine.
-
-					n.EmptyNewAndfaultyPeers()
-				}
+				n.sync()
 			case <-n.stopChan:
 				return
 			}
